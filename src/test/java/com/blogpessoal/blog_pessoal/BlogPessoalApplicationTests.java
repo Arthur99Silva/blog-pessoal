d25d7523987f0b5d3880c8a530b49fb647a6a8fc
Arthur99Silva <arthurassilva99@gmail.com>
2025-04-06 21:13:47 -0300

    Testes sonnar

--- src/test/java/com/blogpessoal/blog_pessoal/BlogPessoalApplicationTests.java
+++ src/test/java/com/blogpessoal/blog_pessoal/BlogPessoalApplicationTests.java
@@ -2,8 +2,10 @@ package com.blogpessoal.blog_pessoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogPessoalApplicationTests {
 
 	@Test
